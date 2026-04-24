a6fdea4aed779045e33c66edff9a82152f868ea9
Shreya C <97959780+ShreyAC962@users.noreply.github.com>
2026-04-23 18:32:11 -0700

    Added search logic to hybrid(text + image) and also made changes to only text and only image logic regarding the search

--- app/api/search.py
+++ app/api/search.py
@@ -1,26 +1,53 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Form
+from PIL import Image
+from io import BytesIO
+
 from app.api.services.embedding_service import get_text_embedding, get_image_embedding
 from app.api.services.pinecone_service import PineconeService
 from app.utils.image_utils import load_image
+from app.utils.fusion import fuse_embeddings
 
 router = APIRouter()
 pinecone_service = PineconeService()
 
+
+# TEXT ONLY SEARCH
 @router.get("/text-search")
 def text_search(query : str):
-    embedding = get_text_embedding(query)
-    results = pinecone_service.query(embedding)
+    text_emb = get_text_embedding(query)
+    final_emb = fuse_embeddings(text_emb=text_emb)
+    results = pinecone_service.query(final_emb)
+    return results
+
+# IMAGE ONLY SEARCH
+@router.post("/image-search")
+async def image_search(file : UploadFile = File(...)):
+    image = load_image(file.file)
+    image_emb = get_image_embedding(image)
+    final_emb = fuse_embeddings(image_emb=image_emb)
+    results = pinecone_service.query(final_emb)
     return results
 
 
-@router.post("/image-search")
-async def image_search(file : UploadFile):
-    image = load_image(file.file)
-    embedding = get_image_embedding(image)
-    results = pinecone_service.query(embedding)
-    return {
-        "embedding_dim" : len(embedding),
-        "results" : results
-    }
+# TEXT + IMAGE SEARCH (HYBRID)
+@router.post("/hybrid-search")
+async def hybrid_search(
+    query: str = Form(None),
+    file: UploadFile = File(None)
+):
+    text_emb = None
+    image_emb = None
+
+    if query:
+        text_emb = get_text_embedding(query)
+    if file:
+        image = load_image(file.file)
+        image_emb = get_image_embedding(image)
+    
+    final_emb = fuse_embeddings(text_emb, image_emb)
+
+    results = pinecone_service.query(final_emb)
+
+    return results
 
 
